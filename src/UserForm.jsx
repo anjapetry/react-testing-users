8b4931425754b068b82e8e807a9f3d17e1724834
Anja Petry <anjapetry@protonmail.com>
2024-01-21 22:16:26 +0100

    feat:add UserForm and UserList plus basic styles

---
+++ src/UserForm.jsx
@@ -0,0 +1,19 @@
+
+
+const UserForm = () => {
+  return (
+  <form>
+    <div>
+        <label>Username</label>
+        <input type="text" />
+        </div>
+        <div>
+            <label>Email</label>
+            <input type="text" />
+        </div>
+        <button>Add User</button>
+    </form>
+  )
+}
+
+export default UserForm;
