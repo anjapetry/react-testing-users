35ec38013e1d2fe590b784340151aca2b881c020
Anja Petry <anjapetry@protonmail.com>
2024-01-22 22:00:09 +0100

    Add event handlers

--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -1,15 +1,23 @@
-
+import { useState } from 'react';
 
 const UserForm = () => {
+  const [email, setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userName, email);
+  }
+
   return (
-  <form>
+  <form onSubmit={handleSubmit}>
     <div>
         <label>Username</label>
-        <input type="text" />
+        <input value={userName} onChange={(e) => setUserName(e.target.value)} type="text" />
         </div>
         <div>
             <label>Email</label>
-            <input type="text" />
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" />
         </div>
         <button>Add User</button>
     </form>
