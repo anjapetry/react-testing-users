76b00a9963ce1a5807e212be85083f68df8f6cde
Anja Petry <anjapetry@protonmail.com>
2024-02-07 21:53:25 +0100

    update deps

--- src/UserForm.test.jsx
+++ src/UserForm.test.jsx
@@ -5,9 +5,11 @@ import UserForm from './UserForm';
 test('it shows two inputs and a button', () => {
     // render the component
     render(<UserForm />);
+
     // manipulate the component or find an element to interact with
 const inputs = screen.getAllByRole('textbox');
 const button = screen.getByRole('button');
+
     // assert on the results of the interaction - what should happen?
 expect(inputs).toHaveLength(2);
 expect(button).toBeInTheDocument();
