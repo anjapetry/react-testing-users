29fb28c749893bcf0c8148380edb22168b7ccb6e
Anja Petry <anjapetry@protonmail.com>
2024-02-10 22:25:09 +0100

    add scree.logTestingPlaygroundURL() to UserList.test.jsx to help with query functions

--- src/__tests__/UserList.test.jsx
+++ src/__tests__/UserList.test.jsx
@@ -4,15 +4,17 @@ import UserList from '../UserList';
 
 test('render one row per user', () => {
     // Arrange: render the component
-    // Act: find all the rows in the table
-    // Assert: ensure correct number of rows
-
   const users = [
     { name: 'Jane', email: 'jane@jane.com' },
     { name: 'John', email: 'johnfawcett@molfarella.io'},
     ];
     render(<UserList users={users} />);
-});
 
+// Act: find all the rows in the table
+ screen.logTestingPlaygroundURL();
+
+
+// Assert: ensure correct number of rows
+});
 test('it shows the name and email of each user', () => {
 });
