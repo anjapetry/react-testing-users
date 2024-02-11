85a69dea868a6d6b7a3cf80066f6fe6a6ef48b83
Anja Petry <anjapetry@protonmail.com>
2024-02-11 22:40:40 +0100

    update UserList.test.jsx to use fallback #2 container.querySelectorAll('tbody tr') to select rows

--- src/__tests__/UserList.test.jsx
+++ src/__tests__/UserList.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import UserList from '../UserList';
 
@@ -8,14 +8,22 @@ test('render one row per user', () => {
     { name: 'Jane', email: 'jane@jane.com' },
     { name: 'John', email: 'johnfawcett@molfarella.io'},
     ];
-    render(<UserList users={users} />);
+    const { container } = render(<UserList users={users} />);
 
 // Act: find all the rows in the table
 // screen.logTestingPlaygroundURL(); to create query functions
-const rows = screen.getAllByRole('row');
+// const rows = screen.getAllByRole('row');
+// workaround to use within to get rows from tbody
+// const rows = within(screen.getByTestId('users')).getAllByRole('row');
+// const table = within(container).getByRole('table');
+// const table = container.querySelector('table');
+// console.log(table);
+// eslint-disable-next-line
+const rows = container.querySelectorAll('tbody tr');
+
 
 // Assert: ensure correct number of rows in the table
-expect(rows).toHaveLength(3);
+expect(rows).toHaveLength(2);
 });
 
 test('it shows the name and email of each user', () => {
