6c84d946759c13809f7d010812b6d17c7d7dc1fe
Anja Petry <anjapetry@protonmail.com>
2024-02-11 21:56:30 +0100

    update UserList.tests.jsx

--- src/__tests__/UserList.test.jsx
+++ src/__tests__/UserList.test.jsx
@@ -11,10 +11,12 @@ test('render one row per user', () => {
     render(<UserList users={users} />);
 
 // Act: find all the rows in the table
- screen.logTestingPlaygroundURL();
+// screen.logTestingPlaygroundURL(); to create query functions
+const rows = screen.getAllByRole('row');
 
-
-// Assert: ensure correct number of rows
+// Assert: ensure correct number of rows in the table
+expect(rows).toHaveLength(3);
 });
+
 test('it shows the name and email of each user', () => {
 });
