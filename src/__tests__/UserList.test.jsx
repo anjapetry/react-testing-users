260fe78868d0154669d8ca67e630b88e5742833a
Anja Petry <anjapetry@protonmail.com>
2024-02-12 21:26:53 +0100

    test table contents

--- src/__tests__/UserList.test.jsx
+++ src/__tests__/UserList.test.jsx
@@ -2,13 +2,22 @@ import { render, screen, within } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import UserList from '../UserList';
 
+function renderComponent() {
+    const users = [
+  { name: 'Jane', email: 'jane@jane.com' },
+  { name: 'John', email: 'johnfawcett@molfarella.io'},
+  ];
+  render(<UserList users={users} />);
+
+  return { users };
+}
+
 test('render one row per user', () => {
     // Arrange: render the component
-  const users = [
-    { name: 'Jane', email: 'jane@jane.com' },
-    { name: 'John', email: 'johnfawcett@molfarella.io'},
-    ];
-    const { container } = render(<UserList users={users} />);
+    renderComponent();
+
+  // const { container } = render(<UserList users={users} />);
+  // render(<UserList users={users} />);
 
 // Act: find all the rows in the table
 // screen.logTestingPlaygroundURL(); to create query functions
@@ -26,5 +35,24 @@ test('render one row per user', () => {
 expect(rows).toHaveLength(2);
 });
 
-test('it shows the name and email of each user', () => {
+test('render the name and email of each user', () => {
+// Arrange: render the component
+const { users } = renderComponent();
+ // const { container } = render(<UserList users={users} />);
+
+// screen.logTestingPlaygroundURL(); then run test and copy URL
+
+for (let user of users) {
+  // Act: find the row for the user
+ // const row = screen.getByText(user.name).closest('tr');
+  // Assert: ensure the row contains the user's name and email
+//  expect(row).toHaveTextContent(user.name);
+//  expect(row).toHaveTextContent(user.email);
+const name = screen.getByRole('cell', { name: user.name });
+const email = screen.getByRole('cell', { name: user.email });
+
+expect(name).toBeInTheDocument();
+expect(email).toBeInTheDocument();
+
+}
 });
